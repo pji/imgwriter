e3651b0fddfe9a3aeeb5183bce35d59d281a0334
Paul J. Iutzi <pji@mac.com>
2021-08-19 07:47:36 -0500

    Added exceptions for file reading problems to read_image.

--- tests/test_imgreader.py
+++ tests/test_imgreader.py
@@ -148,3 +148,33 @@ class ReadImageTestCase(ut.TestCase):
         data as an array.
         """
         self.read_rgb('tiff')
+
+    def test_file_does_not_exist(self):
+        """If given the path of a file that doesn't exist, raise a
+        FileNotFoundError exception.
+        """
+        # Test data and state.
+        filepath = 'tests/data/spam.jpg'
+        
+        # Expected value.
+        exp_ex = FileNotFoundError
+        exp_msg = f'There is no file at {filepath}.'
+        
+        # Run test and determine result.
+        with self.assertRaisesRegex(exp_ex, exp_msg):
+            ir.read_image(filepath)
+    
+    def test_file_not_readable(self):
+        """If given the path of a file that isn't a readable image,
+        raise a ValueError exception.
+        """
+        # Test data and state.
+        filepath = 'tests/data/__test_not_image.txt'
+        
+        # Expected value.
+        exp_ex = ValueError
+        exp_msg = f'The file at {filepath} cannot be read.'
+        
+        # Run test and determine result.
+        with self.assertRaisesRegex(exp_ex, exp_msg):
+            ir.read_image(filepath)
