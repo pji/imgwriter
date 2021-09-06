cf260e0f124c17485722e9d552091b923f06a5f9
Paul J. Iutzi <pji@mac.com>
2021-09-06 09:33:40 -0500

    Updated version number for release.

--- imgwriter/__version__.py
+++ imgwriter/__version__.py
@@ -8,7 +8,7 @@ number.
 
 __title__ = 'imgwriter'
 __description__ = 'A Python package for saving arrays as images or video.'
-__version__ = '0.0.5-dev'
+__version__ = '0.0.5'
 __author__ = 'Paul J. Iutzi'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2021 Paul J. Iutzi'
